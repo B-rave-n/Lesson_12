21699e2187a591bbb23f3ce3537bdb6cd83becd6
Білоцерківець Сергій <sbilocerkivec@gmail.com>
2024-09-22 01:12:44 +0300

    Lesson 12

---
+++ Lesson 12.1.py
@@ -0,0 +1,18 @@
+import re
+
+def delete_html_tags(file_for_cleaned, clean_file):
+    pattern = r'<[^>]+>'
+    file_1 = f'{file_for_cleaned}'
+    file_2 = f'{clean_file}'
+    with open(file_1, 'r') as file:
+        content = file.read()
+        cleaned_content = re.sub(pattern,'', content)
+        lst = cleaned_content.split('\n')
+        result = []
+        for i in lst:
+            if i.strip() != '':
+                result.append(i.strip())
+    with open(file_2, 'w') as cleaned:
+        cleaned.write('\n'.join(result))
+
+delete_html_tags('draft.html', 'cleaned.txt')
